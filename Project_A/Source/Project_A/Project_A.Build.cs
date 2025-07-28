21bea5787f6a04ab95ab8a2afbb2ca9f098f83a8
Alexandr <107064917+etrehappy@users.noreply.github.com>
2025-07-28 21:07:38 +0300

    v_1.0
    pure project

---
+++ Project_A/Source/Project_A/Project_A.Build.cs
@@ -0,0 +1,43 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Project_A : ModuleRules
+{
+	public Project_A(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"Project_A",
+			"Project_A/Variant_Platforming",
+			"Project_A/Variant_Combat",
+			"Project_A/Variant_Combat/AI",
+			"Project_A/Variant_SideScrolling",
+			"Project_A/Variant_SideScrolling/Gameplay",
+			"Project_A/Variant_SideScrolling/AI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Project_A/Source/Project_A/Project_AGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_AGameMode.h"
+
+AProject_AGameMode::AProject_AGameMode()
+{
+	// stub
+}

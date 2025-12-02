80561c92c1b82a27e306d707441b59ec4a89dba4
Aleksandr <107064917+etrehappy@users.noreply.github.com>
2025-12-02 08:29:09 +0300

    Update Project_A.Target.cs

--- Project_A/Source/Project_A.Target.cs
+++ Project_A/Source/Project_A.Target.cs
@@ -8,8 +8,8 @@ public class Project_ATarget : TargetRules
 	public Project_ATarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("Project_A");
 	}
 }

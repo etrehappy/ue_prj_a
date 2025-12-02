7dbfedbbf7fecfb00cd32c259e092c497013403c
Aleksandr <107064917+etrehappy@users.noreply.github.com>
2025-12-02 08:28:45 +0300

    Update Project_AEditor.Target.cs

--- Project_A/Source/Project_AEditor.Target.cs
+++ Project_A/Source/Project_AEditor.Target.cs
@@ -8,8 +8,8 @@ public class Project_AEditorTarget : TargetRules
 	public Project_AEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("Project_A");
 	}
 }

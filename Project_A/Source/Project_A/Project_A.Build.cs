c83a15d055a7669c3630679598be3a5516d2801f
etrehappy <etrehappy@gmail.com>
2025-12-04 14:55:48 +0300

    hw1 complete

--- Project_A/Source/Project_A/Project_A.Build.cs
+++ Project_A/Source/Project_A/Project_A.Build.cs
@@ -17,8 +17,8 @@ public class Project_A : ModuleRules
 			"AIModule",
 			"StateTreeModule",
 			"GameplayStateTreeModule",
-			"UMG" 
-    });
+			"UMG"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] { });
 
@@ -30,20 +30,14 @@ public class Project_A : ModuleRules
 			"Project_A/Variant_SideScrolling",
 			"Project_A/Variant_SideScrolling/Gameplay",
 			"Project_A/Variant_SideScrolling/AI"
-        });
+		});
 
-        if (Target.bBuildEditor)
-        {
-            PrivateDependencyModuleNames.Add("UnrealEd");
-        }
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-    }
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
 }
